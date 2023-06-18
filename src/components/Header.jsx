5a0f35fe70e9bf6cbdc757c1b55e4321ed75e099
anyajegorova <anna.jerm@outlook.com>
2023-06-19 02:10:25 +0300

    header finished

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,35 +6,41 @@ import Account from '../icons/account white.png';
 
 const Header = () => {
     return (
-        <header className='header'>
-            <div className='upper-header_container'>
-                <div className='category-links_container'>
-                    <a href='#'>Italian</a>
-                    <a href='#'>Asian</a>
-                    <a href='#'>American</a>
-                    <a href='#'>Mexican</a>
-                    <a href='#'>French</a>
+        <div>
+            <header className='header'>
+                <div className='upper-header_container'>
+                    <div className='category-links_container'>
+                        <a href='#'>Italian</a>
+                        <a href='#'>Asian</a>
+                        <a href='#'>American</a>
+                        <a href='#'>Mexican</a>
+                        <a href='#'>French</a>
+
+                    </div>
+                    <div className='logo_container'>
+                        <p>Tastybites</p>
+                        <img src={Cook} />
+
+                    </div>
 
                 </div>
-                <div className='logo_container'>
-                    <p>Tastybites</p>
-                    <img src={Cook} />
+                <div className='lower-header_container'>
+                    <div className='menu'>
+                        <a href='#'><img src={Menu} /></a>
+                    </div>
+                    <div className='block_container'>
+                        <div className='text'><p>Unleash your culinary</p> <i>creativity!</i></div>
+
+                    </div>
+                    <div className='account'>
+                        <a href='#'><img src={Favourites} /></a>
+                        <a href='#' style={{ marginLeft: 50 }}><img src={Account} /></a>
+                    </div>
+
 
                 </div>
-
-            </div>
-            <div className='lower-header_container'>
-                <div className='menu'>
-                    <a href='#'><img src={Menu} /></a>
-                </div>
-                <div className='account'>
-                    <a href='#'><img src={Favourites} /></a>
-                    <a href='#'style={{marginLeft: 50}}><img src={Account} /></a>
-                </div>
-
-
-            </div>
-        </header>
+            </header>
+        </div>
     )
 }
 
