97ebd1ee63eeb0927acaf487d3112c2bb47ff322
anyajegorova <anna.jerm@outlook.com>
2023-06-30 20:12:12 +0300

    fixes and card component, api fetch

---
+++ src/components/Card.jsx
@@ -0,0 +1,18 @@
+import './Card.css';
+
+const Card = () => {
+    return (
+        <div className='card-container'>
+            <div className='card '>
+                <div className='card-info_container'>
+
+                </div>
+                <div className='card-image_container'>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
--- src/components/MainDiv.jsx
+++ src/components/MainDiv.jsx
@@ -1,10 +1,41 @@
+import { useState } from 'react';
 import './MainDiv.css';
 
 import RecipeOfTheDay from './RecipeOfTheDay';
 import SearchBar from './SearchBar';
 import Buttons from './Buttons';
+import Card from './Card';
+
+
+const FetchRecipe = async () => {
+    const [start, setStart] = useState(0);
+    const [limit, setLimit] = useState(0);
+
+    const url = `https://yummly2.p.rapidapi.com/feeds/list?limit=${limit}&start=${start}`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '1a4d9c3546msh39469e36c2f4292p19abadjsn53dc465850dd',
+            'X-RapidAPI-Host': 'yummly2.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const result = await response.text();
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const cardCount = () => {
+    const screenWidth = window.innerWidth;
+    return screenWidth >= 1200 ? 6 : screenWidth >= 768 ? 2 : screenWidth >= 480 ? 2 : 1;
+}
 
 const MainDiv = () => {
+
     return (
         <div className='main-div'>
             <div className='image_container'>
@@ -16,6 +47,9 @@ const MainDiv = () => {
                     <RecipeOfTheDay />
                 </div>
 
+            </div>
+            <div className='cards_container'>
+                <Card />
             </div>
         </div>
     )
