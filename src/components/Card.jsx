b472d2ed9b2ce9ff28be97d38e63f12076c7b75a
anyajegorova <anna.jerm@outlook.com>
2023-07-06 21:35:58 +0300

    card component and recipe fetch api

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,37 @@
-import './Card.css';
+import { useState } from 'react';
 
-const Card = ({ name, description, image }) => {
+import './Card.css';
+import Rating from '@mui/material/Rating';
+import Time from '../icons/time.png';
+
+import FavoriteBorderSharpIcon from '@mui/icons-material/FavoriteBorderSharp';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+
+const Card = ({ name, image, category, time, rating }) => {
+    const [like, setLike] = useState(false);
+    const likeToggle = () => {
+        setLike(!like)
+    }
     return (
         <div className='card-container'>
             <div className='card '>
                 <div className='card-info_container'>
+                    <p className='recipe-name'>{name}</p>
+                    <p className='recipe-category'>{category}</p>
+                    <div className='recipe-rating'>
+                        <Rating name='read-only' value={rating} readOnly sx={{ color: 'var(--green-secondary)' , fontSize: 42}}/>
+                    </div>
+                    <div className='cooking-time'>
+                        <img src={Time} alt='cooking time' />
+                        <p>{time}</p>
+                    </div>
 
+                    <div className='like'>
+                        <button onClick={likeToggle}>{like ? <FavoriteBorderSharpIcon sx={{fontSize: 42}}/> : <FavoriteIcon sx={{fontSize: 42}}/>}</button>
+                    </div>
                 </div>
                 <div className='card-image_container'>
-
+                    <img src={image} alt='recipe image' />
                 </div>
             </div>
         </div>
--- src/components/MainDiv.jsx
+++ src/components/MainDiv.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './MainDiv.css';
 
 import RecipeOfTheDay from './RecipeOfTheDay';
@@ -7,15 +7,51 @@ import Buttons from './Buttons';
 import Card from './Card';
 
 
-const FetchRecipe = async () => {
-    const apiKey = process.env.REACT_APP_API_KEY;
+// const FetchRecipe = async () => {
+//     const apiKey = process.env.REACT_APP_API_KEY;
 
-    const [recipies, setRecepies] = useState([]);
+//     const [recipes, setRecipes] = useState([]);
+
+//     const [start, setStart] = useState(0);
+//     const [limit, setLimit] = useState(20);
+
+//     const url = `https://yummly2.p.rapidapi.com/feeds/list?limit=${limit}&start=${start}`;
+//     const options = {
+//         method: 'GET',
+//         headers: {
+//             'X-RapidAPI-Key': apiKey,
+//             'X-RapidAPI-Host': 'yummly2.p.rapidapi.com'
+//         }
+//     };
+
+//     try {
+//         const response = await fetch(url, options);
+//         const result = await response.json();
+//         setRecipes(result);
+//         console.log(result)
+//         console.log(recipes);
+//     } catch (error) {
+//         console.error(error);
+//     }
+//     return recipes;
+// }
+
+const cardCount = () => {
+    const screenWidth = window.innerWidth;
+    return screenWidth >= 1200 ? 6 : screenWidth >= 768 ? 2 : screenWidth >= 480 ? 1 : 1;
+}
+
+const MainDiv = () => {
+
+    const [recipes, setRecipes] = useState({});
+    const [finalRecipe, setFinalRecipe] = useState({});
 
     const [start, setStart] = useState(0);
-    const [limit, setLimit] = useState(0);
+    const [limit, setLimit] = useState(20);
 
     const url = `https://yummly2.p.rapidapi.com/feeds/list?limit=${limit}&start=${start}`;
+    const apiKey = process.env.REACT_APP_API_KEY;
+
     const options = {
         method: 'GET',
         headers: {
@@ -23,22 +59,25 @@ const FetchRecipe = async () => {
             'X-RapidAPI-Host': 'yummly2.p.rapidapi.com'
         }
     };
-
-    try {
-        const response = await fetch(url, options);
-        const result = await response.text();
-        console.log(result);
-    } catch (error) {
-        console.error(error);
+    const fetchRecipe = async () => {
+        try {
+            const response = await fetch(url, options);
+            const recipe = await response.json()
+            setRecipes(recipe);
+            const recipeObject = {'name': recipe.feed[0].content.details.name,'image': recipe.feed[0].display.images[0], 'category': recipe.feed[0].content.details.displayName, 'time': recipe.feed[0].content.details.totalTime, 'rating': recipe.feed[0].content.reviews.averageRating}
+            setFinalRecipe(recipeObject);
+            console.log(finalRecipe)
+        } catch (error) {
+             console.error(error);
+        }
     }
-}
 
-const cardCount = () => {
-    const screenWidth = window.innerWidth;
-    return screenWidth >= 1200 ? 6 : screenWidth >= 768 ? 2 : screenWidth >= 480 ? 2 : 1;
-}
+    useEffect(() => {
+        fetchRecipe();
+    }, []);
 
-const MainDiv = () => {
+    
+
     return (
         <div className='main-div'>
             <div className='image_container'>
@@ -52,7 +91,10 @@ const MainDiv = () => {
 
             </div>
             <div className='cards_container'>
-                <Card />
+                <Card name={finalRecipe.name} image={finalRecipe.image} category={finalRecipe.category} time={finalRecipe.time} rating={finalRecipe.rating}/>
+                {/* {recipes.slice(0, cardCount()).map((recipe, index) => (
+                    <Card key={index} name={''} description={''} image={''} />
+                ))} */}
             </div>
         </div>
     )
